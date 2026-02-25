9a29161ded3b9f774e532e3280710120bdddedc6
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-02-25 15:46:51 -0300

    double or float

--- ex.clj
+++ ex.clj
@@ -19,7 +19,12 @@
         @res)
 
 (defn formula [exp man]
-    (* (math/pow 2 (- exp 1023)) (+ 1 man)))
+    ; 2 ^ numbytes / 2
+    ; double -> 2 ^ 11 / 2 - 1= 2048 / 2 - 1 = 1023
+    ; float -> 2 ^ 8 / 2 - 1= 2048 / 2 - 1 = 127
+    ; see if it is float or double
+    (def offset (if (> exp 255) 1023 127))
+    (* (math/pow 2 (- exp offset)) (+ 1 man)))
 
 (defn bin_to_f [bin]
     (def arr (str/split bin #" "))
@@ -32,4 +37,12 @@
     (printf "%f\n" (if (= (get arr 0 ) "0") res (- res)))
     (println arr))
 
-(bin_to_f "0 10000000110 011001000100000000000000000000000000000000000000000")
+(println "1.a)")
+(bin_to_f "0 01111100 01000000000000000000000")
+(println "1.b)")
+(bin_to_f "0 01111100 01000000000000000000000")
+
+;(println "2.a)")
+;(bin_to_f "0 10000000110 011001000100000000000000000000000000000000000000000")
+;(println "2.ab")
+;(bin_to_f "1 10000000101 001110010000000000000000000000000000000000000000000")
